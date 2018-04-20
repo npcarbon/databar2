f5faae892b258449d948d3ce7a9999db3fd48959
Nattapol <accessoriez0@gmail.com>
2018-04-20 14:27:00 +0700

    อัพโหลดเว็บ2

---
+++ index.php
@@ -0,0 +1,392 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Databar</title>
+<link rel="shortcut icon" href="favicon.png">
+
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/font-awesome.min.css">
+
+<link rel="stylesheet" href="css/magnific-popup.css">
+
+<link rel="stylesheet" href="css/owl.theme.css">
+<link rel="stylesheet" href="css/owl.carousel.css">
+
+<link href='https://fonts.googleapis.com/css?family=Unica+One' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,700' rel='stylesheet' type='text/css'>
+<link href="https://fonts.googleapis.com/css?family=Signika" rel="stylesheet">
+
+<!-- Main css -->
+<link rel="stylesheet" href="css/style1.css">
+<link rel="stylesheet" href="css/cd3-style.css">
+<link rel="stylesheet" href="css/dropbtn.css">
+<link rel="stylesheet" href="css/slideshow.css">
+
+</head>
+<body style="padding-top: 50px;">
+
+
+<!-- PRE LOADER -->
+
+<div class="preloader">
+     <div class="sk-spinner sk-spinner-pulse"></div>
+</div>
+
+
+
+          <div class="visible-md visible-lg">
+               <?php
+                     include 'navlg.inc';
+               ?>
+          </div>
+          <div class="visible-sm visible-xs">               
+               <?php
+                     include 'navsm.inc';
+               ?>
+          </div>
+
+
+<!-- Slide Section -->
+
+<section id="slide" >
+     <div class="container">
+          
+
+
+               <section class="awSlider" >
+                 <div  class="carousel slide" data-ride="carousel">
+                   <!-- Indicators -->
+                   <ol class="carousel-indicators">
+                     <li data-target=".carousel" data-slide-to="0" class="active"></li>
+                     <li data-target=".carousel" data-slide-to="1"></li>
+                     <li data-target=".carousel" data-slide-to="2"></li>
+                     <li data-target=".carousel" data-slide-to="3"></li>
+                   </ol>
+
+                   <!-- Wrapper for slides -->
+                   <div class="carousel-inner" role="listbox">
+                     <div class="item active">
+                       <img src="images/slides/GTA_1.png">
+                       <div class="carousel-caption">#1</div>
+                     </div>
+                     <div class="item">
+                       <img src="images/slides/GTA_2.png">
+                       <div class="carousel-caption">#2</div>
+                     </div>
+                     <div class="item">
+                       <img src="images/slides/GTA_5.png">
+                       <div class="carousel-caption">3</div>
+                     </div>
+                     <div class="item">
+                       <img src="images/slides/GTA_4.png">
+                       <div class="carousel-caption">#4</div>
+                     </div>
+                   </div>
+
+                   <!-- Controls -->
+                   <a class="left carousel-control" href=".carousel" role="button" data-slide="prev">
+                     <span class="sr-only"><</span>
+                   </a>
+                   <a class="right carousel-control" href=".carousel" role="button" data-slide="next">
+                     <span class="sr-only">İleri</span>
+                   </a>
+                 </div>
+               </section>
+                              
+     </div>
+</section>
+<!-- Product Section -->
+
+<section id="product">
+     <div class="overlay">
+          
+     </div>
+     <div class="container">
+          <div class="row">
+
+                <div class="col-md-12 col-sm-12">
+                    <div class="section-title wow fadeInUp">
+                         <h1>Our Products</h1>
+                         <hr>
+                    </div>
+               </div>
+
+               <div class="wow fadeInUp col-md-3 col-sm-6" data-wow-delay="0.4s">
+                    <div class="item wow bounceIn" data-wow-delay="0.1s">
+                         <a href="#">
+                              <img src="images/groups/Group1.jpg" class="img-responsive">
+                         </a>    
+                         <center><button class="btn btn-success section-btn visible-lg visible-md" >View All</button></center>
+                    </div>
+               </div>
+               <div class="wow fadeInUp col-md-3 col-sm-6" data-wow-delay="0.4s">
+                    <div class="item wow fadeInUp" data-wow-delay="0.3s">
+                         <a href="#">
+                              <img src="images/groups/Group2.jpg" class="img-responsive">
+                         </a>
+                         <center><button class="btn btn-success section-btn visible-lg visible-md" >View All</button></center>
+                    </div>
+               </div>
+               <div class="wow fadeInUp col-md-3 col-sm-6" data-wow-delay="0.4s">
+                    <div class="item wow fadeInUp" data-wow-delay="0.5s">
+                          <a href="#">
+                              <img src="images/groups/Group3.jpg" class="img-responsive">
+                          </a>
+                         <center><button class="btn btn-success section-btn visible-lg visible-md">View All</button></center>
+                    </div>
+               </div>
+               <div class="wow fadeInUp col-md-3 col-sm-6" data-wow-delay="0.4s">
+                    <div class="item wow fadeInUp" data-wow-delay="0.7s">
+                         <a href="#">
+                              <img src="images/groups/Group4.jpg" class="img-responsive">
+                         </a>
+                         <center><button class="btn btn-success section-btn visible-lg visible-md">View All</button></center>
+               </div>
+          </div>
+     </div>
+</section>
+
+
+<section id="whychooseus">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-offset-2 col-md-8 col-sm-12">
+                    <div class="wow bounceIn section-title">
+                         <h2>Why Choose Us</h2>
+                         <p class="wow fadeInUp" data-wow-delay="0.5s">We are the Telecommunication Equipment Supplier</p>
+
+                    </div>
+                    <div class="wow fadeInUp product-form" data-wow-delay="0.8s">
+                         <div class="col-md-4 col-xs-12 text-center item-block">
+                              <center><img src="images/icons/tools-icon.png" width="50%" class="img-responsive"></center>
+                              <h2>Service</h2>
+                              <p>อธิบายอย่างคร่าวๆ</p>
+                              <center><button class="btn btn-success section-btn">see more</button></center>
+                         </div>
+                         <div class="col-md-4 col-xs-12 text-center item-block">
+                              <center><img src="images/icons/product.png" width="50%" class="img-responsive"></center>
+                              <h2>Product</h2>
+                              <p>อธิบายอย่างคร่าวๆ</p>
+                              <center><button class="btn btn-success section-btn">see more</button></center>
+                         </div>
+                         <div class="col-md-4 col-xs-12 text-center item-block">
+                              <center><img src="images/icons/product-destruction-company.png" width="50%" class="img-responsive"></center>
+                              <h2>Partners</h2>
+                              <p>อธิบายอย่างคร่าวๆ</p>
+                              <center><button class="btn btn-success section-btn">see more</button></center>
+                         </div>
+                    </div>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+
+
+<!-- Home Section -->
+
+<section id="home" class="main">
+     <div class="overlay"></div>
+	<div class="container">
+		<div class="row">
+
+               <div class="wow fadeInUp col-md-6 col-sm-5 col-xs-10 col-xs-offset-1 col-sm-offset-0" data-wow-delay="0.2s">
+                    <img src="images/home-img.png" class="img-responsive" alt="Home">
+               </div>
+
+               <div class="col-md-6 col-sm-7 col-xs-12">
+                    <div class="home-thumb">
+                         <h1 class="wow fadeInUp" data-wow-delay="0.6s">Our company</h1>
+                         <p class="wow fadeInUp" data-wow-delay="0.8s">The optimal way to present your beautiful mobile app for your startup team. Let us create amazing things!</p>
+                         <a href="#product" class="wow fadeInUp section-btn btn btn-success smoothScroll" data-wow-delay="1s">Download App</a>
+                    </div>
+               </div>
+
+		</div>
+	</div>
+</section>
+
+<!-- product Section -->
+
+
+
+<!-- Screenshot Section -->
+
+<section id="screenshot">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-offset-2 col-md-8 col-sm-12">
+                    <div class="section-title">
+                         <h1>App Screenshots</h1>
+                         <p class="wow fadeInUp" data-wow-delay="0.8s">Nulla nisi purus, ultrices et scelerisque at, ullamcorper et ex. Phasellus at nisi lobortis, semper tortor sed, gravida neque.</p>
+                    </div>
+               </div>
+
+               <!-- Screenshot Owl Carousel -->
+               <div id="screenshot-carousel" class="owl-carousel">
+
+                    <div class="item col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="0.9s">
+                         <a href="images/screenshot-img1.jpg" class="image-popup">
+                              <img src="images/screenshot-img1.jpg" class="img-responsive" alt="screenshot">
+                         </a>
+                    </div>
+
+                    <div class="item col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="0.9s">
+                         <a href="images/screenshot-img2.jpg" class="image-popup">
+                              <img src="images/screenshot-img2.jpg" class="img-responsive" alt="screenshot">
+                         </a>
+                    </div>
+
+                    <div class="item col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="0.9s">
+                         <a href="images/screenshot-img3.jpg" class="image-popup">
+                              <img src="images/screenshot-img3.jpg" class="img-responsive" alt="screenshot">
+                         </a>
+                    </div>
+
+                    <div class="item col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="0.9s">
+                         <a href="images/screenshot-img4.jpg" class="image-popup">
+                              <img src="images/screenshot-img4.jpg" class="img-responsive" alt="screenshot">
+                         </a>
+                    </div>
+
+                    <div class="item col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="0.9s">
+                         <a href="images/screenshot-img5.jpg" class="image-popup">
+                              <img src="images/screenshot-img5.jpg" class="img-responsive" alt="screenshot">
+                         </a>
+                    </div>
+
+                    <div class="item col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="0.9s">
+                         <a href="images/screenshot-img6.jpg" class="image-popup">
+                              <img src="images/screenshot-img6.jpg" class="img-responsive" alt="screenshot">
+                         </a>
+                    </div>
+
+                    <div class="item col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="0.9s">
+                         <a href="images/screenshot-img7.jpg" class="image-popup">
+                              <img src="images/screenshot-img7.jpg" class="img-responsive" alt="screenshot">
+                         </a>
+                    </div>
+
+                    <div class="item col-md-3 col-sm-3 wow fadeInUp" data-wow-delay="0.9s">
+                         <a href="images/screenshot-img8.jpg" class="image-popup">
+                              <img src="images/screenshot-img8.jpg" class="img-responsive" alt="screenshot">
+                         </a>
+                    </div>
+
+               </div>
+
+          </div>
+     </div>
+</section>
+
+
+
+<!-- Divider Section -->
+
+<section id="divider">
+     <div class="container">
+          <div class="row">
+
+               <div class="col-md-offset-2 col-md-8 col-sm-12">
+                    <h2 class="wow fadeInUp" data-wow-delay="0.4s">Praesent tempor nec orci eu condimentum. Vestibulum varius lorem sed odio lacinia, ut efficitur tellus convallis. Phasellus convallis est nisi, sit amet accumsan ipsum elementum quis. Mauris ac sem mi.</h2>
+                    <a href="#screenshot" class="wow fadeInUp section-btn btn btn-success smoothScroll" data-wow-delay="0.8s">Learn More</a>
+               </div>
+
+          </div>
+     </div>
+</section>
+
+
+
+
+<!-- Footer Section -->
+
+<footer>
+	<div class="container">
+		<div class="row">
+
+               <div class="col-md-8 col-sm-6">
+                    <div class="wow fadeInUp footer-copyright" data-wow-delay="0.4s">
+                         <p>Copyright &copy; 2018 Databar co.,Ltd.
+                         </p>
+                    </div>
+               </div>
+
+			<div class="col-md-4 col-sm-6">
+				<ul class="wow fadeInUp social-icon" data-wow-delay="0.8s">
+                         <li><a href="#" class="fa fa-facebook"></a></li>
+                         <li><a href="#" class="fa fa-twitter"></a></li>
+                         <li><a href="#" class="fa fa-google-plus"></a></li>
+                         <li><a href="#" class="fa fa-dribbble"></a></li>
+                         <li><a href="#" class="fa fa-linkedin"></a></li>
+                    </ul>
+               </div>
+			
+		</div>
+	</div>
+</footer>
+
+<!-- Back top -->
+
+<a href="#" class="go-top"><i class="fa fa-angle-up"></i></a>
+
+<!-- Modal Contact -->
+
+<div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+     <div class="modal-dialog">
+      <div class="modal-content modal-popup">
+          <div class="modal-header">
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+               <h2 class="modal-title">Contact Info</h2>
+               <hr>
+          </div>
+          <p style="color: #FFF; font-family: 'Signika', sans-serif;">
+               DATABAR Company Limited 
+          </p>
+          <p style="color: #FFF;">
+               1448/15 Crystal Design Center (CDC), L2 2FL., <br>
+               Room 202. 204. 206.208. <br>
+               Soi Ladprao 87 (Chandra Suk) Praditmanuthum Rd., <br>
+               Klongjan, Bangkapi, Bangkok 10240, <br>
+               THAILAND 
+          </p>
+          <hr>
+          <p style="color: #FFF;">
+          Telphone : +66 2 102 2591 <br>
+          E-mail : sales@databar.co.th      
+          </p>
+<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15498.40180804764!2d100.617811!3d13.802943!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xbe44151e51909d75!2sDATABAR+Company+Limited!5e0!3m2!1sen!2sus!4v1523197242767" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                         
+     </div>
+</div>
+
+
+
+
+<!-- SCRIPTS -->
+
+<script src="js/jquery.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+<script src="js/magnific-popup-options.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/smoothscroll.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
+</html>
