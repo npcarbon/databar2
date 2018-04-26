6e3b5107be6cbd03e07c918260890de4d22b53a5
Nattapol <accessoriez0@gmail.com>
2018-04-26 08:27:43 +0700

    Update savetype.php

--- admin/data/savetype.php
+++ admin/data/savetype.php
@@ -4,23 +4,32 @@ include '../../include/condb.php';
 	$sql = "SELECT * FROM types WHERE Type_Name = '".$_POST['name']."'";
 	$query = @mysqli_query($conn, $sql);
 	$result = @mysqli_fetch_array($query,MYSQLI_ASSOC);
-		if ($result['Type_Name'] != $_POST['name']) {
-			if ( $result['Group_ID'] == "0" ) {
-				# code...
-				echo "<script>alert('Please select The Group of Type');window.history.go(-1);</script>";
-			}else{
-				$sql = "INSERT INTO types (Type_Name, Brand_ID) "
-					. "VALUES ('".$_POST["name"]."',"
-					." '".$_POST["brand"]."')";
-					$result = $conn->query( $sql) or die ("Error in query: $sql " . mysqli_error());
-					
-						echo "<script>alert('Add the new type of product is SUCCESS!');</script>";
-						echo "<meta http-equiv ='refresh'content='0;URL=../addtype.php'>";
+			if ($result['Type_Name'] == $_POST['name'] and $result['Brand_ID'] == $_POST['brand']) {
+				echo "<script>alert('This Brand had been added');window.history.go(-1);</script>";
+				}elseif ($result['Type_Name'] == $_POST['name'] && $result['Brand_ID'] != $_POST['brand']) {
+					$sql = "INSERT INTO types (Type_Name, Brand_ID) "
+						. "VALUES ('".$_POST["name"]."',"
+						." '".$_POST["brand"]."')";
+						$result = @mysqli_query($conn, $sql) or die ("Error in query: $sql " . mysqli_error());
+							
+							echo "<script>alert('Add the new Customer is SUCCESS!');</script>";
+							echo "<meta http-equiv ='refresh'content='0;URL=../addbrand.php'>";
+				}elseif ($result['Type_Name'] != $_POST['name'] && $result['Brand_ID'] == $_POST['brand']) {
+					$sql = "INSERT INTO types (Type_Name, Brand_ID) "
+						. "VALUES ('".$_POST["name"]."',"
+						." '".$_POST["brand"]."')";
+						$result = @mysqli_query($conn, $sql) or die ("Error in query: $sql " . mysqli_error());
+							
+							echo "<script>alert('Add the new Customer is SUCCESS!');</script>";
+							echo "<meta http-equiv ='refresh'content='0;URL=../addbrand.php'>";
+				} else{
+					$sql = "INSERT INTO types (Type_Name, Brand_ID) "
+						. "VALUES ('".$_POST["name"]."',"
+						." '".$_POST["brand"]."')";
+						$result = @mysqli_query($conn, $sql) or die ("Error in query: $sql " . mysqli_error());
+							
+							echo "<script>alert('Add the new Customer is SUCCESS!');</script>";
+							echo "<meta http-equiv ='refresh'content='0;URL=../addbrand.php'>";
+				}
 
-			}
-		}else{
-				echo "<script>alert('The type of product has been added');window.history.go(-1);</script>";
-				// header('location:addcus.php');}
-			}
-		// header('location:addcus.php');
-			$conn->close();
+$conn->close();
